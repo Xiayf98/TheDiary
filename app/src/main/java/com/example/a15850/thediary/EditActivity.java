b7d76ebb9f3669235af730f795f6313ebad854c7
Xiayf98 <13773313800@163.com>
2019-04-27 17:00:29 +0800

    将标题框和正文框中获取到的内容传入对应的字符串

--- app/src/main/java/com/example/a15850/thediary/EditActivity.java
+++ app/src/main/java/com/example/a15850/thediary/EditActivity.java
@@ -13,9 +13,10 @@ import com.robinhood.ticker.TickerView;
 
 public class EditActivity extends AppCompatActivity {
 
-
-    private EditText editText;
-    String inputText;
+    private EditText editTextTitle;
+    private EditText editTextBody;
+    String inputTextTitle;
+    String inputTextBody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +36,18 @@ public class EditActivity extends AppCompatActivity {
         });
 
         //EditText
-        editText = (EditText) findViewById(R.id.editText_edit);
+        editTextTitle = (EditText) findViewById(R.id.editText_editTitle);
+        editTextBody=(EditText)findViewById(R.id.editText_editBody);
         //Floating Button
         FloatingActionButton button_EditToMain = (FloatingActionButton) findViewById(R.id.mainButton_edit);
         FloatingActionButton button_UndoToMain = (FloatingActionButton) findViewById(R.id.mainButton_undo);
 
-
+        //发送键的响应函数，分别将标题和正文传入inputTextTitle和inputTextBody
         button_EditToMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                inputText = editText.getText().toString();
+                inputTextTitle = editTextTitle.getText().toString();
+                inputTextBody = editTextBody.getText().toString();
                 // Toast.makeText(EditActivity.this,inputText,Toast.LENGTH_SHORT).show(); //测试EditText内容传入了inputText//测试成功
                 Intent intent_edit_main = new Intent(EditActivity.this,MainActivity.class);
                 startActivity(intent_edit_main);
